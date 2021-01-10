61413d86925ad8e774d3594348f302938e420484
BendaoudIsmael <73161081+BendaoudIsmael@users.noreply.github.com>
2021-01-10 20:30:39 +0100

    Database with libaries
    setting up Database with libaries.

---
+++ IsmaelBendaoudAssignment/IsmaelBendaoudAssignment/PL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsmaelBendaoudAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("=========");
+
+            Console.ReadKey();
+        }
+    }
+}
